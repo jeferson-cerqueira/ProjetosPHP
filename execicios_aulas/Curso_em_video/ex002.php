7046c94412b98fbe2e8cd4c4406319bc9eb9519b
jeferson cerqueira <130681426+jeferson-cerqueira@users.noreply.github.com>
2024-09-03 18:01:06 -0300

    utilizando variaveis no PHP

---
+++ execicios_aulas/Curso_em_video/ex002.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="PT-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>testando variaveis</title>
+</head>
+<body>
+    
+<h1>Testando variaveis</h1>
+
+<?php 
+ // var
+    $name = "Jeferson";
+    $course = "PHP";
+    $teacher= "Gustavo Guanabara";
+    $site = "curso em video";
+
+    // presentation
+   echo "meu nome é $name, estou fazendo o curso de $course, com o professor $teacher , do $site ";
+?>
+
+</body>
+</html>
